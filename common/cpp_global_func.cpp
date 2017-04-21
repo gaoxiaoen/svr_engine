78db256aef176a73f39d3cf0a0a5bf56b3a4b884
gaoxiaoen <648789030@qq.com>
2017-04-21 11:14:44 +0800

    add daemon process

--- common/cpp_global_func.cpp
+++ common/cpp_global_func.cpp
@@ -18,3 +18,33 @@
 #define LUA_FALSE "false"
 
 
+void daemon_init(const char *_deamon_wdir)
+{
+	pid_t pid;
+	signal(SIGCHLD,SIG_IGN);
+	if((pid ==fork())){
+		exit(0);    //父进程退出
+	}
+	else if(pid <0)
+	{
+		printf("fork1 failed(%s)\n",strerror(errno));
+	}
+	setsid();
+	if ((pid = fork()))
+	{
+		exit(0);
+	}
+	else if(pid <0)
+	{
+		printf("fork2 failed(%s)\n",strerror(errno));
+	}
+
+	for(int i=0;i<getdtablesize();i++)
+		close(i);
+	umask(0);
+
+	int std_fd = open("dev/null",O_RDWR);
+	dup2(std_fd,STDERR_FILENO);
+	dup2(std_fd,STDIN_FILENO);
+	dup2(std_fd,STDOUT_FILENO);
+}
